e86dfb39bfcbf5c1c8fb35390269785e8e336d1f
Jhamil <jhamil.bravo@unmsm.edu.pe>
2023-09-02 01:24:31 -0500

    comentarios UDP

--- src/UDP/ClienteUDP.java
+++ src/UDP/ClienteUDP.java
@@ -6,9 +6,13 @@ import java.net.*;
  public class ClienteUDP {
     public static void main(String[] args) {
         try {
+            //Crea flujo de entrada
             BufferedReader doUsuario = new BufferedReader(new InputStreamReader(System.in));
-
+            
+            //Crea socket de cliente
             DatagramSocket socketCliente = new DatagramSocket();
+            
+            //traduce nombre host a la dirección IP usando dns
             InetAddress IPAddress = InetAddress.getByName("localhost"); 
             byte[] sendData = new byte[1024];;
             byte[] receiveData = new byte[1024];
@@ -17,12 +21,17 @@ import java.net.*;
             String frase = doUsuario.readLine();
             sendData = frase.getBytes();
 
+            
             System.out.println("Enviando paquete...");
+            //Crea datagrama con datos para enviar longitud, direccion, ip, puerta
             DatagramPacket paqueteEnviado = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
             socketCliente.send(paqueteEnviado);
             
+            
             System.out.println("Recibiendo paquete...");
+            //enviar datagrama al servidor
             DatagramPacket paqueteRecibido = new DatagramPacket(receiveData, receiveData.length);
+            //lee datagrama del servidor
             socketCliente.receive(paqueteRecibido);
             
             
--- src/UDP/ServidorUDP.java
+++ src/UDP/ServidorUDP.java
@@ -6,6 +6,7 @@ import java.net.*;
 public class ServidorUDP {
     public static void main(String[] args) {
         try {
+            //crea socket para datagrama en la puerta 9876
             DatagramSocket socketServidor = new DatagramSocket(9876);
             byte[] datosRecibidos = new byte[1024];
             byte[] datosEnviados = new byte[1024];
@@ -13,6 +14,7 @@ public class ServidorUDP {
             System.out.println("Servidor esperando conexiones...");
 
             while (true) {
+                //asigname memoria para recibir datagrama
                 DatagramPacket paqueteRecibido = new DatagramPacket(datosRecibidos, datosRecibidos.length);
 
                 socketServidor.receive(paqueteRecibido);
@@ -20,19 +22,22 @@ public class ServidorUDP {
                 String frase = new String(paqueteRecibido.getData());
                 System.out.println("Mensaje recibido del cliente: " + frase);
                 
+                //Obtiene dirección IP, no. de puerta del remitente
                 InetAddress IPAddress = paqueteRecibido.getAddress();
                 int port = paqueteRecibido.getPort();
 
                 String fraseEnMayusculas = frase.toUpperCase();
 
                 datosEnviados = fraseEnMayusculas.getBytes();
-
+                //Crea datagrama para enviar al cliente
                 DatagramPacket paqueteEnviado = new DatagramPacket(datosEnviados, datosEnviados.length, IPAddress, port);
 
+                //Escribe datagrama al socket
                 socketServidor.send(paqueteEnviado);
-
                 
                 System.out.println("Mensaje enviado al cliente: " + fraseEnMayusculas);
+
+                //Fin del while, vuelve al inicio y espera la llegada de otro datagrama
             }
         } catch (IOException e) {
             e.printStackTrace();
